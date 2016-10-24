c486a64deb4a9d1635e1e254aaf7a6a668632e62
Jonathan Lange <jml@mumak.net>
2016-10-24 19:42:21 +0100

    Annotate things to fix / address

--- src/GraphQL/Muckaround.hs
+++ src/GraphQL/Muckaround.hs
@@ -108,13 +108,19 @@ data (name :: k) :> a deriving (Typeable)
 
 -- XXX: This structure is cargo-culted from Servant, even though jml doesn't fully
 -- understand it yet.
+--
+-- XXX: Rename "Handler" to "Resolver"? Resolver is a technical term within
+-- GraphQL, so only use it if it matches exactly.
 type Handler = IO
+-- XXX: Rename to Graph & GraphT?
 type Server api = ServerT api Handler
 
 class HasGraph api where
   type ServerT api (m :: * -> *) :: *
   resolve :: Proxy api -> Server api -> Application
 
+-- XXX: GraphQL responses don't *have* to be JSON. See 'Response'
+-- documentation for more details.
 data GetJSON (t :: *)
 
 runQuery :: HasGraph api => Proxy api -> Server api -> CanonicalQuery -> IO Response
@@ -140,10 +146,12 @@ instance (KnownSymbol name, HasGraph api) => HasGraph (name :> api) where
     where
       fieldName = toS (symbolVal (Proxy :: Proxy name))
       -- XXX: What to do if there are argumentS?
+      -- XXX: This is almost certainly partial.
       lookup q f = listToMaybe [ (a, s) | AST.SelectionField (AST.Field a n [] _ s) <- q
                                         , n == f
                                         ]
       buildField alias' value = do
         value' <- value
-        -- XXX: An object? Really?
+        -- XXX: An object? Really? jml thinks this should just be a key/value
+        -- pair and that some other layer should assemble an object.
         pure (Aeson.object [alias' .= value'])
