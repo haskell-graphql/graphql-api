6abb2c63de4ff14d62fa0f33ac7dbe3590014e6a
Tom Hunger <tehunger@gmail.com>
2016-12-06 21:48:59 +0000

    Implement changes discussed with jml.

--- src/GraphQL/MuckTom.hs
+++ src/GraphQL/MuckTom.hs
@@ -237,6 +237,7 @@ instance forall ks sl. (KnownSymbol ks, GetSymbolList sl) => HasAnnotatedInputTy
 --   - that excludes callback because we could forget to callback
 --   - unless we can figure out a way to return a unique value from callback?
 -- * no super-crazy type magic
+
 --   - that excludes vinyl
 -- * nested tuples make it hard to write single-tuple entries (Identity or Only like in postgresql-simple could work?)
 --   - nested tuples need unpacking to be usable
@@ -254,36 +255,26 @@ type instance FieldName (Field ks t) = ks
 type instance FieldName (Argument a0 a1 :> a) = FieldName a
 
 -- simple text placeholder query for now
-type Query = [Text]
+type QueryTerm = Text
 type QueryArgs = [Text]
-
--- One of the constraints of the query is that we return fields in the
--- same order as queried. I.e. evaluation is query-directed, and
--- handlers need to be invoked accordingly. That means we need to
--- build a map of MonadIO thunks that can be run as needed in the
--- evaluation. That's why we have this intermediate ValueOrSubmap.
---
--- Because we're query driven we only look at the query fragment when
--- examining, so the query needs to be available.
-data ValueOrSubmap' m =
-    VValue (Value.Value)
-  | VSubmap ((M.Map Text (m ValueOrSubmap)))
-type ValueOrSubmap = ValueOrSubmap' IO
+type Query = [QueryTerm] -- also know as a `SelectionSet`
 
 class HasGraph a where
   type HandlerType a
-  buildResolver :: HandlerType a -> IO ValueOrSubmap
+  buildResolver :: HandlerType a -> Query -> IO Value.Value
 
 -- TODO not super hot on individual values having to be instances of
 -- HasGraph but not sure how else we can nest either types or
 -- (Object _ _ fields). Maybe instead of field we need a "SubObject"?
 instance HasGraph Int32 where
   type HandlerType Int32 = IO Int32
-  buildResolver handler =  fmap (VValue . (Value.toValue)) handler
+  -- TODO check that selectionset is empty (we expect a terminal node)
+  buildResolver handler _ =  fmap Value.toValue handler
 
 instance HasGraph Double where
   type HandlerType Double = IO Double
-  buildResolver handler =  fmap (VValue . (Value.toValue)) handler
+  -- TODO check that selectionset is empty (we expect a terminal node)
+  buildResolver handler _ =  fmap Value.toValue handler
 
 -- Parse a value of the right type from an argument
 class ReadValue a where
@@ -301,47 +292,51 @@ instance ReadValue Double where
 
 class BuildFieldResolver a where
   type FieldHandler a :: Type
-  buildFieldResolver :: FieldHandler a -> (Text, IO ValueOrSubmap)
+  buildFieldResolver :: FieldHandler a -> QueryArgs -> (Text, IO Value.Value)
+
 
 instance forall ks t. (KnownSymbol ks, HasGraph t) => BuildFieldResolver (Field ks t) where
   type FieldHandler (Field ks t) = HandlerType t
-  buildFieldResolver handler =
-    let childResolver = buildResolver @t handler -- need query access here...
+  buildFieldResolver handler queryArgs =
+    let childResolver = buildResolver @t handler [] -- need sub query access so can't just pass queryargs
         name = toS (symbolVal (Proxy :: Proxy ks))
     in (name, childResolver)
 
--- Fundamental problem: queryArgs are not available yet when I run
--- buildFieldResolver because they depend on the actual part of the
--- query I am evaluating right now.
 instance forall ks t f. (KnownSymbol ks, BuildFieldResolver f, ReadValue t) => BuildFieldResolver (Argument ks t :> f) where
   type FieldHandler (Argument ks t :> f) = t -> FieldHandler f
-  buildFieldResolver handler =
+  buildFieldResolver handler queryArgs =
     let partiallyAppliedHandler = handler (readValue @t []) -- need query args access here
-    in buildFieldResolver @f partiallyAppliedHandler
+    in buildFieldResolver @f partiallyAppliedHandler queryArgs
 
 
 class RunFields a where
   type RunFieldsType a :: Type
-  runFields :: RunFieldsType a -> ValueOrSubmap
+  -- Runfield is run on a single QueryTerm so it can only ever return
+  -- one (Text, Value)
+  runFields :: RunFieldsType a -> QueryTerm -> IO (Text, Value.Value)
+
 
 instance forall f fs.
          ( BuildFieldResolver f
          , RunFields fs
          ) => RunFields (f:fs) where
   type RunFieldsType (f:fs) = (FieldHandler f) :<> (RunFieldsType fs)
-
   -- Deconstruct object type signature and handler value at the same
   -- time and run type-directed code for each field.
-  runFields handler@(lh :<> rh) =
-    let (k, v) = buildFieldResolver @f lh
-        mapped = case runFields @fs rh of
-                   VSubmap m -> VSubmap (M.insert k v m)
-                   _ -> error "unexpected non VSubmap value - programming error."
-    in mapped
+  runFields handler@(lh :<> rh) term =
+    let (k, valueIO) = buildFieldResolver @f lh [] -- TODO query args
+    in case term == k of
+      True -> do
+        value <- valueIO
+        pure (k, value)
+      False -> runFields @fs rh term
+
 
 instance RunFields '[] where
   type RunFieldsType '[] = ()
-  runFields _ = VSubmap M.empty
+  -- TODO maybe better to return Either?
+  runFields _ term = error ("Query for undefined term:" <> (show term))
+
 
 -- TODO: arguments. The interpreter
 instance forall typeName interfaces fields.
@@ -349,31 +344,17 @@ instance forall typeName interfaces fields.
          ) => HasGraph (Object typeName interfaces fields) where
   type HandlerType (Object typeName interfaces fields) = IO (RunFieldsType fields)
 
-  buildResolver handler = do
-    h <- handler
-    -- TODO handler should run in MonadIO so it can run do e.g. dbQueries
-    pure $ runFields @fields h
-{-e
+  buildResolver handlerIO query = do
+    -- First we run the actual handler function itself in IO.
+    handler <- handlerIO
+    -- considering that this is an object we'll need to collect (name,
+    -- Value) pairs from runFields and build a map with them.
 
-evalFirst :: IO ()
-evalFirst = do
-  let (name, vsm') = buildResolver @T tHandler
-  vsm <- vsm'
-  print name
+    -- might need https://hackage.haskell.org/package/linkedhashmap to
+    -- keep insertion order. (TODO)
+    r <- forM query $ \term -> runFields @fields handler term
+    pure $ Value.toValue $ M.fromList r
 
-eval :: Query -> IO ValueOrSubmap -> IO ()
-eval (q:qs) vsm = do
-  value <- vsm
-  case value of
-    -- the "object case".
-    VSubmap thunk -> do
-      let submap = thunk [q]
-      eval [q] submap -- lookup?
-      print "submap"
-    VValue thunk -> do
-      let value = thunk [q]
-      print value
--}
 
 --- test code below
 type T = Object "T" '[] '[Field "z" Int32, Argument "t" Int :> Field "t" Int32]
@@ -383,8 +364,8 @@ tHandler = do
   conn <- print @IO @Text "HI"
   pure $ (pure 10) :<> (\tArg -> pure 10) :<> ()
 
-hlist :: a -> (a :<> ())
-hlist a = a :<> ()
+-- hlist :: a -> (a :<> ()) TODO
+-- hlist a = a :<> ()
 
 {-
 type Calculator = Object "Calculator" '[]
