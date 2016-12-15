3206905e944abac15c8bc561dfebd27e0589cb5d
Tom Hunger <tehunger@gmail.com>
2016-12-15 15:09:41 +0000

    Add example code for union types.

---
+++ examples/UnionExample.hs
@@ -0,0 +1,32 @@
+module UnionExample where
+
+import Protolude hiding (Enum)
+import GraphQL.Definitions
+import GraphQL.TypeApi
+import qualified Data.GraphQL.AST as AST
+import Data.GraphQL.Parser (document)
+import GraphQL.Value (Value)
+import Data.Attoparsec.Text (parseOnly, endOfInput)
+
+type O1 = Object "O1" '[] '[Field "o1" Text]
+type O2 = Object "O2" '[] '[Field "o2" Text]
+
+type T = Union "U" '[O1, O2]
+
+o1 :: HandlerType IO O1
+o1 = pure ((pure "hello from O1") :<> ())
+
+o2 :: HandlerType IO O2
+o2 = pure ((pure "hello from O2") :<> ())
+
+tHandler :: HandlerType IO T
+tHandler = o1 :<|> o2 :<|> ()
+
+exampleQuery :: IO Value
+exampleQuery = buildResolver @IO @T tHandler (query "{ ... on O1 { o1 } ... on O2 { o2 } }")
+
+query :: Text -> AST.SelectionSet
+query q =
+  let Right (AST.Document [AST.DefinitionOperation (AST.Query (AST.Node _ _ _ selectionSet))]) =
+       parseOnly (document <* endOfInput) q
+  in selectionSet
