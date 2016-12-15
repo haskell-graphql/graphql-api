b20b928f6c95cbf070573ee8687cd9919251e5ec
Jonathan Lange <jml@mumak.net>
2016-12-15 19:38:21 +0000

    Move validation tests to separate module

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,17 +1,12 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TypeOperators #-}
 module Main
   ( main
   ) where
 
 import Protolude
 
-import Test.Tasty (defaultMain, TestTree, testGroup)
-import Test.Tasty.Hspec (testSpec, describe, it, shouldBe)
+import Test.Tasty (defaultMain, testGroup)
 
-import qualified Data.GraphQL.AST as AST
-import GraphQL.Validation (ValidationError(..), getErrors)
+import qualified ValidationTests
 import qualified TypeTests
 import qualified TypeApiTests
 
@@ -20,42 +15,8 @@ import Examples.UnionExample ()
 
 main :: IO ()
 main = do
-  t <- sequence [ tests
+  t <- sequence [ ValidationTests.tests
                 , TypeTests.tests
                 , TypeApiTests.tests
                 ]
   defaultMain (testGroup "spec" t)
-
-tests :: IO TestTree
-tests = testSpec "GraphQL API" $ do
-  describe "Validation" $ do
-    it "Treats simple queries as valid" $ do
-      let doc = AST.Document
-                [ AST.DefinitionOperation
-                  ( AST.Query
-                    ( AST.Node "me" [] []
-                      [ AST.SelectionField (AST.Field "name" "name" [] [] [])
-                      ]
-                    )
-                  )
-                ]
-      getErrors doc `shouldBe` []
-
-    it "Detects duplicate operation names" $ do
-      let doc = AST.Document
-                [ AST.DefinitionOperation
-                  ( AST.Query
-                    ( AST.Node "me" [] []
-                      [ AST.SelectionField (AST.Field "name" "name" [] [] [])
-                      ]
-                    )
-                  )
-                , AST.DefinitionOperation
-                  ( AST.Query
-                    ( AST.Node "me" [] []
-                      [ AST.SelectionField (AST.Field "name" "name" [] [] [])
-                      ]
-                    )
-                  )
-                ]
-      getErrors doc `shouldBe` [DuplicateOperation "me"]
