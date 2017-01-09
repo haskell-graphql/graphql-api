2d5f2f77b268bb3b8860ecbb2f22ffdc52a55794
Tom Hunger <tehunger@gmail.com>
2017-01-09 10:51:10 +0000

    Better names and fillers where it makes sense.

--- src/GraphQL/API/Enum.hs
+++ src/GraphQL/API/Enum.hs
@@ -20,69 +20,69 @@ import GHC.Generics (M1(..), Rep, Meta(..), C1, U1(..), D, (:+:)(..))
 import GHC.TypeLits (KnownSymbol, TypeError, ErrorMessage(..))
 
 
-class GenricEnumValues (r :: Type -> Type) where
+class GenricEnumValues (f :: Type -> Type) where
   genericEnumValues :: [Name]
-  genericEnumFromValue :: GValue.Value -> Either Text (r p)
-  genericEnumToValue :: r p -> GValue.Value
+  genericEnumFromValue :: GValue.Value -> Either Text (f p)
+  genericEnumToValue :: f p -> GValue.Value
 
-instance forall n m p f nt.
-  ( KnownSymbol n
+instance forall conName m p f nt.
+  ( KnownSymbol conName
   , KnownSymbol m
   , KnownSymbol p
   , GenricEnumValues f
-  ) => GenricEnumValues (M1 D ('MetaData n m p nt) f) where
+  ) => GenricEnumValues (M1 D ('MetaData conName m p nt) f) where
   genericEnumValues = genericEnumValues @f
   genericEnumFromValue v@(GValue.ValueEnum _) = M1 <$> genericEnumFromValue v
   genericEnumFromValue x = Left ("Not an enum: " <> show x)
   genericEnumToValue (M1 gv) = genericEnumToValue gv
 
-instance forall n f p b.
-  ( KnownSymbol n
+instance forall conName f p b.
+  ( KnownSymbol conName
   , GenricEnumValues f
-  ) => GenricEnumValues (C1 ('MetaCons n p b) U1 :+: f) where
-  genericEnumValues = let Right name = nameFromSymbol @n in name:(genericEnumValues @f)
+  ) => GenricEnumValues (C1 ('MetaCons conName p b) U1 :+: f) where
+  genericEnumValues = let Right name = nameFromSymbol @conName in name:(genericEnumValues @f)
   genericEnumFromValue v@(GValue.ValueEnum vname) =
-    case nameFromSymbol @n of
+    case nameFromSymbol @conName of
       Right name -> if name == vname
                     then L1 <$> Right (M1 U1)
                     else R1 <$> genericEnumFromValue v
       Left x -> Left ("Not a valid enum name: " <> show x)
   genericEnumFromValue _ = panic "This case should have been caught at top-level. Please file a bug."
   genericEnumToValue (L1 _) =
-    let Right name = nameFromSymbol @n
+    let Right name = nameFromSymbol @conName
     in GValue.ValueEnum name
   genericEnumToValue (R1 gv) = genericEnumToValue gv
 
-instance forall n p b. (KnownSymbol n) => GenricEnumValues (C1 ('MetaCons n p b) U1) where
-  genericEnumValues = let Right name = nameFromSymbol @n in [name]
+instance forall conName p b. (KnownSymbol conName) => GenricEnumValues (C1 ('MetaCons conName p b) U1) where
+  genericEnumValues = let Right name = nameFromSymbol @conName in [name]
   genericEnumFromValue (GValue.ValueEnum vname) =
-    case nameFromSymbol @n of
+    case nameFromSymbol @conName of
       Right name -> if name == vname
                     then Right (M1 U1)
                     else Left ("Not a valid enum name: " <> show vname)
       Left x -> Left ("Not a valid enum name: " <> show x)
   genericEnumFromValue _ = panic "This case should have been caught at top-level. Please file a bug."
   genericEnumToValue (M1 _) =
-    let Right name = nameFromSymbol @n
+    let Right name = nameFromSymbol @conName
     in GValue.ValueEnum name
 
 -- TODO(tom): better type errors using `n`. Also type errors for other
 -- invalid constructors.
-instance forall n p b sa sb.
-  ( TypeError ('Text "Constructor not unary: " ':<>: 'Text n)
-  , KnownSymbol n
-  ) => GenricEnumValues (C1 ('MetaCons n p b) (S1 sa sb)) where
-  genericEnumValues = undefined
-  genericEnumFromValue = undefined
-  genericEnumToValue = undefined
+instance forall conName p b sa sb.
+  ( TypeError ('Text "Constructor not unary: " ':<>: 'Text conName)
+  , KnownSymbol conName
+  ) => GenricEnumValues (C1 ('MetaCons conName p b) (S1 sa sb)) where
+  genericEnumValues = notImplemented
+  genericEnumFromValue = notImplemented
+  genericEnumToValue = notImplemented
 
-instance forall n p b sa sb f.
-  ( TypeError ('Text "Constructor not unary: " ':<>: 'Text n)
-  , KnownSymbol n
-  ) => GenricEnumValues (C1 ('MetaCons n p b) (S1 sa sb) :+: f) where
-  genericEnumValues = undefined
-  genericEnumFromValue = undefined
-  genericEnumToValue = undefined
+instance forall conName p b sa sb f.
+  ( TypeError ('Text "Constructor not unary: " ':<>: 'Text conName)
+  , KnownSymbol conName
+  ) => GenricEnumValues (C1 ('MetaCons conName p b) (S1 sa sb) :+: f) where
+  genericEnumValues = notImplemented
+  genericEnumFromValue = notImplemented
+  genericEnumToValue = notImplemented
 
 
 -- | For each enum type we need 1) a list of all possible values 2) a
