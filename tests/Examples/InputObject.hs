35c461784c6d57ce274388160a00a080ae8fcdc3
Jonathan Lange <jml@mumak.net>
2017-01-16 22:27:13 +0000

    Update examples to use new top-level APIs

--- tests/Examples/InputObject.hs
+++ tests/Examples/InputObject.hs
@@ -7,8 +7,7 @@ import Protolude hiding (Enum)
 
 import GraphQL
 import GraphQL.API
-import GraphQL.Resolver (Handler, Result(..), (:<>)(..), buildResolver, Defaultable(..))
-import GraphQL.Value (Value)
+import GraphQL.Resolver (Handler, Defaultable(..))
 import GraphQL.Value.FromValue (FromValue)
 
 data DogStuff = DogStuff { toy :: Text, likesTreats :: Bool } deriving (Show, Generic)
@@ -26,26 +25,25 @@ root :: Handler IO Query
 root = pure (\dogStuff -> pure (show dogStuff))
 
 -- $setup
--- >>> import qualified GraphQL.Internal.Encoder as Encode
--- >>> import GraphQL.Value (valueToAST)
+-- >>> import Data.Aeson (encode)
+-- >>> import GraphQL.Value.ToValue (ToValue(..))
+
+-- TODO: jml thinks it's a bit confusing to have `show` output in these
+-- examples. Mixing between JSON syntax and Haskell's record syntax confuses
+-- the point.
 
 -- | Show input object usage
 --
--- >>> (Result _ result) <- example
--- >>> putStrLn $ Encode.value (valueToAST result)
--- {root:"DogStuff {toy = \"bone\", likesTreats = True}"}
-example :: IO (Result Value)
-example = buildResolver @IO @Query root (query "{ root(dogStuff: {toy: \"bone\", likesTreats: true}) }")
+-- >>> response <- example
+-- >>> putStrLn $ encode $ toValue response
+-- {"data":{"root":"DogStuff {toy = \"bone\", likesTreats = True}"}}
+example :: IO Response
+example = interpretAnonymousQuery @Query root "{ root(dogStuff: {toy: \"bone\", likesTreats: true}) }"
 
 -- | Show that example replacement works
 --
--- >>> (Result _ result) <- exampleDefault
--- >>> putStrLn $ Encode.value (valueToAST result)
--- {root:"DogStuff {toy = \"shoe\", likesTreats = False}"}
-exampleDefault :: IO (Result Value)
-exampleDefault = buildResolver @IO @Query root (query "{ root }")
-
-query :: Text -> SelectionSet Value
-query q = either (panic . show) identity $ do
-  document <- compileQuery q
-  getOperation document Nothing mempty
+-- >>> response <- exampleDefault
+-- >>> putStrLn $ encode $ toValue response
+-- {"data":{"root":"DogStuff {toy = \"shoe\", likesTreats = False}"}}
+exampleDefault :: IO Response
+exampleDefault = interpretAnonymousQuery @Query root "{ root }"
--- tests/Examples/UnionExample.hs
+++ tests/Examples/UnionExample.hs
@@ -1,19 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 module Examples.UnionExample  where
 
-import Protolude hiding (Enum)
-import qualified GraphQL.Internal.Validation as Validation
-import GraphQL.API
-import GraphQL (compileQuery, getOperation)
-import GraphQL.Resolver
-import GraphQL.Value (Value)
+import Protolude
+import GraphQL.API (Field, List, Object, Union)
+import GraphQL (Response, interpretAnonymousQuery)
+import GraphQL.Resolver (Handler, (:<>)(..), unionValue)
 
 -- Slightly reduced example from the spec
 type MiniCat = Object "MiniCat" '[] '[Field "name" Text, Field "meowVolume" Int32]
 type MiniDog = Object "MiniDog" '[] '[Field "barkVolume" Int32]
 
 type CatOrDog = Union "CatOrDog" '[MiniCat, MiniDog]
-type CatOrDogList = List (Union "CatOrDog" '[MiniCat, MiniDog])
+type CatOrDogList = Object "CatOrDogList" '[] '[Field "pets" (List (Union "CatOrDog" '[MiniCat, MiniDog]))]
 
 miniCat :: Text -> Handler IO MiniCat
 miniCat name = pure (pure name :<> pure 32)
@@ -27,35 +25,28 @@ catOrDog = do
   unionValue @MiniCat (miniCat name)
 
 catOrDogList :: Handler IO CatOrDogList
-catOrDogList = pure
-  [ unionValue @MiniCat (miniCat "Felix")
-  , unionValue @MiniCat (miniCat "Mini")
-  , unionValue @MiniDog miniDog
-  ]
+catOrDogList = pure $
+  pure [ unionValue @MiniCat (miniCat "Felix")
+       , unionValue @MiniCat (miniCat "Mini")
+       , unionValue @MiniDog miniDog
+       ]
 
 -- $setup
--- >>> import qualified GraphQL.Internal.Encoder as Encode
--- >>> import GraphQL.Value (valueToAST)
-
+-- >>> import Data.Aeson (encode)
+-- >>> import GraphQL.Value.ToValue (ToValue(..))
 
 -- | Show usage of a single unionValue
 --
--- >>> (Result _ result) <- exampleQuery
--- >>> putStrLn $ Encode.value (valueToAST result)
--- {name:"MonadicFelix",meowVolume:32}
-exampleQuery :: IO (Result Value)
-exampleQuery = buildResolver @IO @CatOrDog catOrDog (query "{ ... on MiniCat { name meowVolume } ... on MiniDog { barkVolume } }")
+-- >>> response <- exampleQuery
+-- >>> putStrLn $ encode $ toValue response
+-- {"data":{"meowVolume":32,"name":"MonadicFelix"}}
+exampleQuery :: IO Response
+exampleQuery = interpretAnonymousQuery @CatOrDog catOrDog "{ ... on MiniCat { name meowVolume } ... on MiniDog { barkVolume } }"
 
 -- | 'unionValue' can be used in a list context
 --
--- >>> (Result _ result) <- exampleListQuery
--- >>> putStrLn $ Encode.value (valueToAST result)
--- [{name:"Felix",meowVolume:32},{name:"Mini",meowVolume:32},{barkVolume:100}]
-exampleListQuery :: IO (Result Value)
-exampleListQuery = buildResolver @IO @CatOrDogList catOrDogList  (query "{ ... on MiniCat { name meowVolume } ... on MiniDog { barkVolume } }")
-
-query :: Text -> Validation.SelectionSet Value
-query q =
-  let Right doc = compileQuery q
-      Right x = getOperation doc Nothing mempty
-  in x
+-- >>> response <- exampleListQuery
+-- >>> putStrLn $ encode $ toValue response
+-- {"data":{"pets":[{"meowVolume":32,"name":"Felix"},{"meowVolume":32,"name":"Mini"},{"barkVolume":100}]}}
+exampleListQuery :: IO Response
+exampleListQuery = interpretAnonymousQuery @CatOrDogList catOrDogList  "{ pets { ... on MiniCat { name meowVolume } ... on MiniDog { barkVolume } } }"
