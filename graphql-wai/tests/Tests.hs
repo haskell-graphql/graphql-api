79684a5006709428b6a1c38cc35e4570923ef85c
Tom Hunger <tehunger@gmail.com>
2017-01-27 14:25:57 +0000

    Wai Application adpater.

---
+++ graphql-wai/tests/Tests.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds #-} -- , FlexibleContexts, TypeFamilies, AllowAmbiguousTypes #-}
+module Main where
+
+import Protolude
+
+import Network.Wai.Test
+import GraphQL.API
+import GraphQL.Wai
+import GraphQL.Resolver
+
+type Cat = Object "Cat" '[] '[Field "name" Text]
+
+catHandler :: Handler IO Cat
+catHandler = pure (pure "Felix")
+
+test1 :: Session ()
+test1 = do
+  r <- request $ setPath defaultRequest "/?query={ name }"
+  assertStatus 200 r
+  assertBody "{\"data\":{\"name\":\"Felix\"}}" r
+
+main :: IO ()
+main = do
+  void $ runSession test1 (toApplication @Cat catHandler)
