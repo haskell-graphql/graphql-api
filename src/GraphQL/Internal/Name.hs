0a96e0033c5222b2cc0ed5c9e368f2c63f83f4dc
Paul Desmond Parker <pauldesmondparker@gmail.com>
2017-12-23 12:13:35 +0800

    Cut commented out code. Comment monoid instance.
    Following suggestions made at:
    [pull/139#discussion_r158537349](https://github.com/jml/graphql-api/pull/139#discussion_r158537349)
    and
    [pull/139#discussion_r158537230](https://github.com/jml/graphql-api/pull/139#discussion_r158537230)
    and
    [pull/139#discussion_r158537382](https://github.com/jml/graphql-api/pull/139#discussion_r158537382)

--- src/GraphQL/Internal/Name.hs
+++ src/GraphQL/Internal/Name.hs
@@ -36,19 +36,14 @@ import GraphQL.Internal.Syntax.Tokens (tok)
 -- https://facebook.github.io/graphql/#sec-Names
 newtype Name = Name { unName :: T.Text } deriving (Eq, Ord, Show)
 
+-- | Allow Name to be parsed with `optempty`
+--
+-- Example: node = AST.Node <$> optempty nameParser
+-- I.e. If nameParser fails, the Name field of AST.Node is set
+-- mempty rather than propagating a failure. 
 instance Monoid Name where
     mempty  = Name T.empty
---    mappend (Name {a}) mempty = Name {a}
---    mappend mempty (Name {b}) = Name {b}
     mappend (Name a1) (Name a2) = Name (T.append a1 a2)
---    mappend = append
---    mconcat = concat
-
---newtype Any = Any { getAny :: Bool }
-
---instance Monoid Any where
---    mempty = Any False
---    (Any b1) `mappend` (Any b2) = Any (b1 || b2)
 
 -- | Create a 'Name', panicking if the given text is invalid.
 --
